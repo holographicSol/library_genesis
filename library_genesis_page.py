303defc2e64435477405e74731243ab0916992e2
holographic_sol <88397088+holographicSol@users.noreply.github.com>
2022-10-02 06:57:39 +0100

    Add files via upload

---
+++ library_genesis_page.py
@@ -0,0 +1,135 @@
+"""
+Written by Benjamin Jack Cullen aka Holographic_Sol
+"""
+import os
+from pylibgen import Library
+import requests
+from bs4 import BeautifulSoup
+import urllib.request
+
+failed = []
+ids_ = []
+cloud_flare_found_ = []
+
+
+def enter_query():
+    search_q = input('download using search query: ')
+    f_dir = './library_genesis/' + search_q + '/'
+    i_page = input('enter page number: ')
+    i_page = int(i_page)
+    compile_ids(i_page, f_dir, search_q)
+
+
+def compile_ids(i_page, f_dir, search_q):
+
+    ids = []
+    l = Library()
+    try:
+        ids = l.search(query=search_q, mode='title', page=i_page, per_page=100)
+        print('[page:', i_page, '] book IDs:', ids)
+        for _ in ids:
+            if _ not in ids_:
+                ids_.append(_)
+    except Exception as e:
+        print(e)
+
+    lookup_ids = l.lookup(ids_)
+    print('found:', len(ids_), 'books')
+
+    if ids_:
+        user_dl_books = input('would you like to download?(y/n): ')
+        if user_dl_books == 'y' or user_dl_books == 'Y':
+            if not os.path.exists(f_dir):
+                os.mkdir(f_dir)
+            dl_books(f_dir, lookup_ids)
+
+
+def dl_books(f_dir, lookup_ids):
+    i = 0
+    for _ in lookup_ids:
+        try:
+            i += 1
+            print('-' * 100)
+            print('progress:', i, '/', len(ids_))
+            # print(_.__dict__)
+
+            title = _.title
+            if title == '':
+                title = str(i) + '_unknown_title'
+            print('title:', _.title)
+
+            author = _.author
+            if author == '':
+                author = str(i) + '_unknown_author'
+            print('author:', _.author)
+
+            year = _.year
+            if year == '':
+                year = str(i) + '_unknown_year'
+            print('year:', _.year)
+
+            md5 = _.md5
+            if md5 == '':
+                print('-- could not find md5, skipping')
+                failed.append([title, author, year])
+                break
+            print('md5:', _.md5)
+
+            url = ('http://library.lol/main/' + str(md5))
+            rHead = requests.get(url)
+            data = rHead.text
+            soup = BeautifulSoup(data, "html.parser")
+            cloud_flare_found = False
+            for link in soup.find_all('a'):
+                href = (link.get('href'))
+                if href.endswith(tuple(['.pdf', '.djvu', '.ebub', '.azw3', '.mobi'])):
+
+                    if href.endswith('.pdf'):
+                        ext = '.pdf'
+                    elif href.endswith('.djvu'):
+                        ext = '.djvu'
+                    elif href.endswith('.ebub'):
+                        ext = '.ebub'
+                    elif href.endswith('.azw3'):
+                        ext = '.azw3'
+                    elif href.endswith('.mobi'):
+                        ext = '.mobi'
+
+                    print('download:', href)
+
+                    if 'cloudflare' in href or 'http://library.lol/' in href:
+                        print('extension:', ext)
+                        print('download:', href)
+
+                        save_path = f_dir + "".join([c for c in title if c.isalpha() or c.isdigit() or c == ' ']).rstrip()
+                        save_path = save_path + ' (by ' + "".join([c for c in author if c.isalpha() or c.isdigit() or c == ' ']).rstrip()
+                        save_path = save_path + ' ' + "".join([c for c in year if c.isalpha() or c.isdigit() or c == ' ']).rstrip() + ')' + ext
+
+                        if not os.path.exists(save_path):
+                            print('saving file:', save_path)
+                            try:
+                                urllib.request.urlretrieve(href, save_path)
+                            except Exception as e:
+                                print(e)
+                                failed.append([title, author, year, href])
+                        else:
+                            print('-- skipping:', href)
+
+        except Exception as e:
+            print(e)
+            try:
+                failed.append([_.title, _.author, _.year, href])
+            except Exception as e:
+                print(e)
+
+
+enter_query()
+
+print('-' * 100)
+if failed:
+    print('\nmay have failed (check to confirm):')
+    for _ in failed:
+        print('   ', _)
+    print('may have failed:', len(failed))
+
+print('-- completed.')
